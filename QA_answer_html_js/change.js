c1c1b910deac3e30df1c611b3a166f02b698397c
Bodya95 <x.bodya@bk.ru>
2018-02-17 21:19:01 +0200

    two blocks

---
+++ QA_answer_html_js/change.js
@@ -0,0 +1,12 @@
+var block = document.getElementById('block_2');
+block.onclick = function () {
+	this.style.background = 'black';
+	this.style.width = 300 + 'px';
+	this.style.height = 300 + 'px';
+	block_1.style.display = 'none'
+}
+/*
+var block = document.getElementById('block_1');
+block.onclick = function () {
+	this.style.display = 'none';
+}*/
